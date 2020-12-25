e054ed2c1fba44b0bc6ea5c7f2f9c8ee15432d39
sebastientromp <sebastien.tromp@gmail.com>
2020-12-25 14:44:21 +0100

    remove core module to add a dependency on the main unityspy repo
    this repo is kept only for the UI

--- src/HackF5.UnitySpy.HearthstoneLib/Detail/Battlegrounds/BattlegroundsInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HackF5.UnitySpy.HearthstoneLib.Detail.Battlegrounds
-{
-    internal class BattlegroundsInfo : IBattlegroundsInfo
-    {
-        public int Rating { get; set; }
-
-        public int PreviousRating { get; set; }
-    }
-}
